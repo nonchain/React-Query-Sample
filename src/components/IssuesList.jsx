cda96a4a51f9c9d791820cb986752b2b5725c18a
nonchain <code.progali@gamil.com>
2023-01-03 09:09:35 +0330

    Create Issue List

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
 
-export default function IssuesList() {
+import IssueItem from "./IssueItem";
+
+export default function IssuesList({ labels, status }) {
+  const issuesQuery = useQuery(
+    ['issues', { labels, status }],
+    () => {
+      const statusString = status ? `&status=${status}` : '';
+      const labelString = labels.map(label => `labels[]=${label}`).join("&");
+      return fetch(`/api/issues?${labelString}${statusString}`).then(res => res.json())
+    }
+  );
+
+  console.log(issuesQuery?.data);
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2 >Issues List</h2 >
+      {issuesQuery.isLoading ? <p>Loading...</p> : <ul className="issues-list">
+        {issuesQuery?.data?.map(issue => <IssueItem
+          key={issue?.id}
+          title={issue?.title}
+          number={issue?.number}
+          assignee={issue?.assignee}
+          createdBy={issue?.createdBy}
+          createdDate={issue?.createdDate}
+          commentCount={issue?.comments?.length}
+          labels={issue?.labels}
+          status={issue.status} />
+        )}
+      </ul>}
     </div>
   );
 }
