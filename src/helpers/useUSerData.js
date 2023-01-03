c4a8c713f740e055687b95a99f19ed0626340195
nonchain <code.progali@gamil.com>
2023-01-03 09:08:15 +0330

    Use a custom hook to fetch users data by id

---
+++ src/helpers/useUSerData.js
@@ -0,0 +1,12 @@
+import { useQuery } from "react-query";
+
+// Access User data in entire app
+export function useUserData(userId) {
+   const userData = useQuery(
+      // when userId changed, refetch data
+      ["users", userId],
+      () => fetch(`/api/users/${userId}`).then(res => res.json())
+   );
+
+   return userData;
+}
