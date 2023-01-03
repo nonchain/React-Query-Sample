324ee4f79d2b628bbfaafe922aacaafe1bbec09f
nonchain <code.progali@gamil.com>
2023-01-03 09:18:08 +0330

    Show labels and status filters

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
+import StatusSelected from "../components/StatusSelected";
 export default function Issues() {
+  const [labels, setLabels] = useState([]);
+  const [status, setStatus] = useState("");
+
   return (
     <div>
       <main>
         <section>
           <h1>Issues</h1>
-          <IssuesList />
+          <IssuesList labels={labels} status={status}/>
         </section>
         <aside>
-          <LabelList />
+          <LabelList
+            selectedLabel={labels}
+            toggle={(label) => 
+              setLabels(currentLabels => 
+                currentLabels.includes(label) ?
+              currentLabels.filter(currentLabel => currentLabel !== label) : currentLabels.concat(label))
+            } />
+
+            <h3>Status</h3>
+            <StatusSelected 
+            value={status}
+            onChange={(e) => setStatus(e?.target?.value)}/>
         </aside>
       </main>
     </div>
