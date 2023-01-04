ef318cd829b124da225f3ed04b90e01145b324ce
nonchain <code.progali@gamil.com>
2023-01-04 08:21:54 +0330

    Import Issue Header

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,7 +1,24 @@
+import React from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import IssueHeader from "./IssueHeader";
+
+function useIssueData(issueNumber) {
+  return useQuery(
+    ["issues", issueNumber],
+    () => fetch(`/api/issues/${issueNumber}`).then(res => res.json())
+  )
+}
 
 export default function IssueDetails() {
   const { number } = useParams();
+  const issueQuery= useIssueData(number);
 
-  return <h1>Issue {number}</h1>;
+  return <div className="issue-details">
+    {
+      issueQuery.isLoading ? <p>Loading...</p> : <React.Fragment>
+        <IssueHeader {...issueQuery.data} />
+      </React.Fragment>
+    }
+  </div>;
 }
