733451fb6ed9d70a196bea648ef84a86645474a1
nonchain <code.progali@gamil.com>
2023-01-08 15:49:48 +0330

    Desing body

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import IssueHeader from "./IssueHeader";
+import Comment from "./Comment";
 
 function useIssueData(issueNumber) {
   return useQuery(
@@ -10,14 +11,37 @@ function useIssueData(issueNumber) {
   )
 }
 
+export function useIssueComment(issueNumber) {
+  const userData = useQuery(
+    ["issues", issueNumber, "comments"],
+    () => fetch(`/api/issues/${issueNumber}/comments`).then(res => res.json())
+  );
+
+  return userData;
+}
+
 export default function IssueDetails() {
   const { number } = useParams();
-  const issueQuery= useIssueData(number);
+  const issueQuery = useIssueData(number);
+  const commentsQuery = useIssueComment(number);
 
   return <div className="issue-details">
     {
       issueQuery.isLoading ? <p>Loading...</p> : <React.Fragment>
         <IssueHeader {...issueQuery.data} />
+        <main>
+          <section>
+            {commentsQuery.isLoading ?
+              <p>Loading...</p> :
+              commentsQuery?.data?.map(comment =>
+                <Comment key={comment?.id} {...comment} />
+              )
+            }
+          </section>
+          <aside>
+            
+          </aside>
+        </main>
       </React.Fragment>
     }
   </div>;
